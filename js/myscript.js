42d405cefb7fe668486d63f27011c48c921d754d
JayVadher <jayvadher_91@hotmail.com>
2017-08-01 11:18:10 +0800

    MyRecipes
    A simple webpage with a sliding in sidebar menu and  aligned content

---
+++ js/myscript.js
@@ -0,0 +1,15 @@
+function openSlideMenu(){
+    document.getElementById('sidebar').style.width = '250px';
+    document.getElementById('main').style.marginLeft = '250px';
+
+}
+
+function closeSlideMenu(){
+	document.getElementById('sidebar').style.width = '0';
+    document.getElementById('main').style.margin = '0 auto';
+}
+ 
+
+
+
+
